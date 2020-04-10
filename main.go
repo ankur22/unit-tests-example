692a7cf169ed99b3290710cfe936195de0d67e58
ankur22 <akahank@gmail.com>
2020-04-10 16:31:50 +0100

    Add html and cli methods of shortening the url. Add integration tests too. Ensure the URLShortner is thread safe

--- main.go
+++ main.go
@@ -1,20 +1,171 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
 	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-// URLShortner will return a shortened url of the input url
-// e.g. https://google.com/search?q=blah -> urls.com/12
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	u := &URLShortner{
+		i:     0,
+		store: make(map[string]string),
+	}
+
+	c := make(chan string)
+	o := make(chan string)
+	defer close(c)
+	defer close(o)
+	go cliInput(ctx, c, o)
+	go cliFronted(ctx, u, c, o)
+
+	go httpFronted(ctx, u)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT)
+
+	done := make(chan bool, 1)
+	go func() {
+		<-sig
+		fmt.Println("Shutting down service")
+		done <- true
+	}()
+
+	fmt.Println("Started")
+	<-done
+	cancel()
+	fmt.Println("Exiting")
+}
+
+func cliInput(ctx context.Context, c chan<- string, o <-chan string) {
+	go func() {
+		for {
+			var in string
+			_, err := fmt.Scanln(&in)
+			if err != nil {
+				fmt.Printf("Caught error %v\n", err)
+				continue
+			}
+			c <- in
+			out := <-o
+			fmt.Printf("Shotened URL: %s\n", out)
+		}
+	}()
+
+	<-ctx.Done()
+	fmt.Printf("Close cliInput\n")
+}
+
+func cliFronted(ctx context.Context, u *URLShortner, c <-chan string, o chan<- string) {
+	go func() {
+	outer:
+		for {
+			select {
+			case in := <-c:
+				resp, err := u.Shorten(in)
+				if err != nil {
+					fmt.Printf("Caught error %v\n", err)
+					break
+				}
+				o <- resp
+			case <-ctx.Done():
+				fmt.Printf("Close cliFrontend\n")
+				break outer
+			}
+		}
+	}()
+}
+
+type body struct {
+	Index string
+}
+
+func httpFronted(ctx context.Context, u *URLShortner) {
+	s := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		r := mux.NewRouter()
+
+		r.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+			url := r.FormValue("u")
+
+			resp, err := u.Shorten(url)
+			if err != nil {
+				w.WriteHeader(400)
+				w.Write([]byte(fmt.Sprintf("Not a valid URL '%s'\n", url)))
+				return
+			}
+
+			b, err := json.Marshal(body{Index: resp})
+			if err != nil {
+				fmt.Printf("Error while marshalling json %v\n", err)
+				w.WriteHeader(500)
+				return
+			}
+
+			w.WriteHeader(201)
+			w.Header().Add("content-type", "application/json")
+			w.Write(b)
+		}).Queries("u", "{url}").Methods("GET")
+
+		r.HandleFunc("/{i}", func(w http.ResponseWriter, r *http.Request) {
+			vars := mux.Vars(r)
+			i := vars["i"]
+
+			resp, err := u.GetURL(i)
+			if err != nil {
+				w.WriteHeader(404)
+				w.Write([]byte(fmt.Sprintf("Not found '%s'\n", i)))
+				return
+			}
+
+			http.Redirect(w, r, resp, 302)
+		})
+
+		s.Handler = r
+
+		fmt.Println("Starting server")
+		err := s.ListenAndServe()
+		if err != nil {
+			fmt.Printf("Error while trying to start server %v\n", err)
+		}
+
+		fmt.Println("Server shutting down")
+	}()
+
+	<-ctx.Done()
+	fmt.Println("Shut down server")
+	s.Shutdown(ctx)
+	fmt.Printf("Close httpFrontend\n")
+}
+
+// URLShortner can create ids for the original
+// URLs and return the original URLs with the id
 type URLShortner struct {
+	m     sync.Mutex
 	store map[string]string
 	i     int
 }
 
-// Shorten will convert urls to a shortened url
-// e.g. https://google.com/search?q=blah -> urls.com/12
+// Shorten will convert urls to a id
+// e.g. https://google.com/search?q=blah -> 12
 func (u *URLShortner) Shorten(in string) (string, error) {
 	if !strings.Contains(in, ".com") {
 		return "", errors.New("invalid")
@@ -24,15 +175,19 @@ func (u *URLShortner) Shorten(in string) (string, error) {
 		in = fmt.Sprintf("https://%s", in)
 	}
 
+	var val string
+	u.m.Lock()
 	u.i++
-	val := fmt.Sprintf("%d", u.i)
+	val = strconv.Itoa(u.i)
 	u.store[val] = in
+	u.m.Unlock()
 
 	return val, nil
 }
 
-// Get the full URL with the shortened URL
-func (u *URLShortner) Get(in string) (string, error) {
+// GetURL the full URL with the id
+// e.g. 12 -> https://google.com/search?q=blah
+func (u *URLShortner) GetURL(in string) (string, error) {
 	val, ok := u.store[in]
 	if !ok {
 		return "", errors.New("Not found")
--- main_test.go
+++ main_test.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -46,14 +53,107 @@ func TestURLShortner(t *testing.T) {
 	}
 
 	for _, test := range tests2 {
-		t.Run("Get"+test.name, func(t *testing.T) {
+		t.Run("GetURL"+test.name, func(t *testing.T) {
 			s := &URLShortner{i: 0, store: make(map[string]string)}
 
 			s.Shorten(test.in1)
-			resp, err := s.Get(test.in2)
+			resp, err := s.GetURL(test.in2)
 
 			assert.Equal(t, test.err, err)
 			assert.Equal(t, test.expected, resp)
 		})
 	}
+
+	u := URLShortner{store: make(map[string]string), i: 0}
+
+	t.Run("Race", func(t *testing.T) {
+		l := 1000
+		var wg sync.WaitGroup
+		for i := 0; i < l; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err := u.Shorten("google.com")
+				assert.NoError(t, err)
+			}()
+		}
+		wg.Wait()
+
+		resp, err := u.Shorten("google.com")
+		assert.NoError(t, err)
+		assert.Equal(t, strconv.Itoa(l+1), resp)
+	})
+}
+
+func BenchmarkURLShortner(b *testing.B) {
+	u := URLShortner{store: make(map[string]string), i: 0}
+
+	b.Run("Race", func(b *testing.B) {
+		l := 1000
+		var wg sync.WaitGroup
+		for i := 0; i < l; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err := u.Shorten("google.com")
+				assert.NoError(b, err)
+			}()
+		}
+		wg.Wait()
+	})
+}
+
+func TestCLI(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	u := &URLShortner{i: 0, store: make(map[string]string)}
+
+	o := make(chan string)
+	c := make(chan string)
+	defer close(c)
+	defer close(o)
+	go cliFronted(ctx, u, c, o)
+
+	c <- "google.com"
+	out := <-o
+
+	actual, err := u.GetURL(out)
+	assert.Equal(t, "https://google.com", actual)
+	assert.NoError(t, err)
+
+	actual, err = u.GetURL("foo")
+	assert.Error(t, err)
+
+	cancel()
+	time.Sleep(1 * time.Second)
+}
+
+func TestHTTP(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	u := &URLShortner{i: 0, store: make(map[string]string)}
+
+	go httpFronted(ctx, u)
+
+	c := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := c.Get("http://localhost:8080/1")
+	assert.NoError(t, err)
+	assert.Equal(t, 404, resp.StatusCode)
+
+	resp, err = c.Get("http://localhost:8080/shorten?u=http:google.com")
+	assert.NoError(t, err)
+	assert.Equal(t, 201, resp.StatusCode)
+
+	b, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	body := &body{}
+	err = json.Unmarshal(b, body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "1", body.Index)
+
+	cancel()
 }
